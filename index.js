10d2faef2bff6cb79fe9f06143a67b26be2af03b
rabby6530 <you@example.com>
2023-05-22 19:27:15 +0600

    install cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
 const chef = require('./data/chef.json');
 const recipe = require('./data/foodRecipes.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome to my server side')
