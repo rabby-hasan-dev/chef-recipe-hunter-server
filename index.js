c55c84ce3fa0331037405b74cea4051bf2f49d05
rabby6530 <you@example.com>
2023-05-22 12:03:44 +0600

    added chef data

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chef=require('./data/chef');
 
 app.get('/', (req, res) => {
     res.send('Welcome to my server side')
 });
+
+app.get('/chef',(req,res)=>{
+    res.send(chef)
+})
 
 app.listen(port, () => {
     console.log(`Chef Recipe hunter server port on${port}`)
